16f1d3535007ff335663a7ac7405824b6a35eeeb
Naama Atias <naamazch@gmail.com>
2021-12-08 11:35:04 +0200

    atlas

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://NaamaA:inaarat9@cluster0.m1jap.mongodb.net/nia_db?retryWrites=true&w=majority',
 }
